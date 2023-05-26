74be0f1ba369b26d4853026a4f9f7e34d4915d4a
Jeffrey <562475805lanu@gmail.com>
2023-05-26 18:33:40 +1000

    now book will auto return after 7 day

--- functions/checkBookStatus.php
+++ functions/checkBookStatus.php
@@ -1,4 +1,16 @@
 <?php
+include("../conn.php");
+
+$current_datetime = date("Y-m-d H:i:s");
+
+$sql = "UPDATE book_status
+SET Member_id=null, Status='Available', Applied_date='$current_datetime'
+WHERE Applied_date <= now() - INTERVAL 7 day";
+
+mysqli_query($conn, $sql);
+
+
+
 $sql = "SELECT * FROM book_status";
 $res = mysqli_query($conn, $sql);
 
